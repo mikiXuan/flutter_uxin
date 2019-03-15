086a998be88d6d5067c17e24e6b4d3ffea6f213c
xinwenxuan <362793861@qq.com>
2019-03-15 19:52:03 +0800

    测试提交代码3

---
+++ lib/Application/login/loginUI.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget{
+  LoginView({Key key}) : super(key: key);
+
+  @override
+  _LoginViewState createState() => new _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView>
+{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
+
