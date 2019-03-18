f05d234cb1769d46cc36e963ddfcee6bfbbc054d
xinwenxuan <362793861@qq.com>
2019-03-18 17:35:16 +0800

    测试提交代码3

--- lib/Application/login/loginUI.dart
+++ lib/Application/login/loginUI.dart
@@ -12,7 +12,36 @@ class _LoginViewState extends State<LoginView>
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return null;
+    return new Scaffold(
+      body: new Center(
+        child: new Padding(
+          padding: const EdgeInsets.only(top: 240),
+          child: new Column(
+            children: <Widget>[
+              new Text(
+                '优教信使登陆',
+                style: new TextStyle(
+                  fontSize: 68.0,
+                  fontFamily: "Billabong",
+                  color: Colors.black),
+                ),
+              new Padding(padding: const EdgeInsets.only(bottom: 100.0)),
+              new GestureDetector(
+                onTap: login,
+                child: new Image.asset(
+                  "assets/images/google_signin_button.png",
+                  width: 225,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void login() async {
+
   }
 }
 
