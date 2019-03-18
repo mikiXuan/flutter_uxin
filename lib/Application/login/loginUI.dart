1d6a61484dbac01d5205f54fb4f35b6c0feaf438
xinwenxuan <362793861@qq.com>
2019-03-18 19:23:00 +0800

    测试提交代码4

--- lib/Application/login/loginUI.dart
+++ lib/Application/login/loginUI.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class LoginView extends StatefulWidget{
+  final String userName = '';
+  final String userPwd = '';
   LoginView({Key key}) : super(key: key);
 
   @override
@@ -13,35 +15,76 @@ class _LoginViewState extends State<LoginView>
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-      body: new Center(
-        child: new Padding(
-          padding: const EdgeInsets.only(top: 240),
-          child: new Column(
-            children: <Widget>[
-              new Text(
-                '优教信使登陆',
-                style: new TextStyle(
-                  fontSize: 68.0,
-                  fontFamily: "Billabong",
-                  color: Colors.black),
+      appBar: null,
+      body:GestureDetector(
+        behavior: HitTestBehavior.translucent,
+        onTap: (){
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.all(25.0),
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                new Padding(padding: const EdgeInsets.only(bottom: 150.0)),
+                new Text(
+                  '优教信使登陆',
+                  style: new TextStyle(
+                      fontSize:50.0,
+                      fontFamily: "Billabong",
+                      color: Colors.black),
                 ),
-              new Padding(padding: const EdgeInsets.only(bottom: 100.0)),
-              new GestureDetector(
-                onTap: login,
-                child: new Image.asset(
-                  "assets/images/google_signin_button.png",
-                  width: 225,
+                new Padding(padding: const EdgeInsets.only(bottom: 50.0)),
+                new TextField(
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(10.0),
+                    icon: null,
+                    labelText: '请输入用户名',
+                    helperText: '手机号/账号',
+                  ),
+//                  onChanged: _userNameChanged,
+                  onSubmitted: _userNameChanged,
+                  autofocus: false,
                 ),
-              )
-            ],
-          ),
-        ),
+                new TextField(
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(10.0),
+                    icon: null,
+                    labelText: '请输入密码',
+                    helperText: '初始123456的那个密码',
+                  ),
+//                  onChanged: _userPwdChanged,
+                  onSubmitted: _userPwdChanged,
+                  autofocus: false,
+                ),
+                new Padding(padding: const EdgeInsets.only(bottom: 50.0)),
+                new GestureDetector(
+                  onTap: login,
+                  child: new Image.asset(
+                    "assets/images/btn_Tea_GuidePage@2x.png",
+                    width: 125,
+                  ),
+                )
+              ],
+            )
+          ],
+        )
       ),
     );
   }
 
-  void login() async {
 
+  void login() async {
+    print('点击了立即进入');
+  }
+  void _userNameChanged(String str) {
+    print(str);
+  }
+  void _userPwdChanged(String str) {
+    print(str);
   }
 }
 
